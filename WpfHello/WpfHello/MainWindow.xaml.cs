81beffdc578fda8c35456c9f81f158c33eec94b6
Vsevolod Kosarev <iamvkosarev@gmail.com>
2024-03-25 02:16:05 +0800

    Сделал WPF пр 1 упр 1-2

---
+++ WpfHello/WpfHello/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isDataDirty = false;
+        public MyWindow myWin { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbl.Content = "Добрый день!";
+            setBut.IsEnabled = false;
+            retBut.IsEnabled = false;
+            Top = 25;
+            Left = 25;
+
+
+        }
+
+        private void setBut_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.StreamWriter sw = null;
+            try
+            {
+                sw = new System.IO.StreamWriter("username.txt");
+                sw.WriteLine(setText.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sw != null)
+                    sw.Close();
+            }
+            retBut.IsEnabled = true;
+            isDataDirty = false;
+        }
+
+        private void retBut_Click(object sender, RoutedEventArgs e)
+        {
+            System.IO.StreamReader sr = null;
+            try
+            {
+                using (sr = new System.IO.StreamReader("username.txt"))
+                    retLabel.Content = "Приветствую Вас, уважаемый " + sr.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+            }
+        }
+
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            setBut.IsEnabled = true;
+            isDataDirty = true;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (this.isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения ? ";
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                if (result != MessageBoxResult.No)
+                {
+                    return;
+                }
+                e.Cancel = true;
+            }
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void New_Win_Click(object sender, RoutedEventArgs e)
+        {
+            if (myWin == null)
+                myWin = new MyWindow();
+
+            var location = New_Win.PointToScreen(new Point(0, 0));
+            myWin.Left = location.X + New_Win.Width;
+            myWin.Top = location.Y;
+
+            myWin.Owner = this;
+            myWin.Show();
+        }
+    }
+}
---
+++ WpfHello/WpfHello/MyWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfHello
+{
+    /// <summary>
+    /// Логика взаимодействия для MyWindow.xaml
+    /// </summary>
+    public partial class MyWindow : Window
+    {
+        private bool _close;
+        MainWindow wnd1 = null;
+
+
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+        public new void Close()
+        {
+            _close = true;
+            base.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            wnd1.myWin = null;
+
+            if (_close) return;
+            e.Cancel = true;
+            Hide();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            wnd1 = Owner as MainWindow;
+            if (wnd1 != null)
+            {
+                wnd1.txtBlock.Text = textBox.Text;
+                PrintLogFile();
+            }
+            Close();
+        }
+
+        private void PrintLogFile()
+        {
+            using System.IO.StreamWriter writer = new System.IO.StreamWriter("log.txt", true);
+            writer.WriteLine($"Внесено \"{textBox.Text}\": {DateTime.Now.ToShortDateString() + ", время: " + DateTime.Now.ToLongTimeString()} ");
+            writer.Flush();
+
+        }
+    }
+}

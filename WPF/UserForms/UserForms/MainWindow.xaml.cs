3445720f6baafe652af2bfdfb5d470706dc397b6
Vsevolod Kosarev <iamvkosarev@gmail.com>
2024-04-12 01:48:47 +0800

    Сделал пр. 10
    Немного сложно, но полезно понимать как можно собирать из отдельных функциональных частей окна

---
+++ WPF/UserForms/UserForms/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms.Integration;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserForms
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> PhoneNumbers = new List<String>();
+        System.Windows.Forms.SaveFileDialog aDialog;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.MaskedTextBox aBox;
+            aBox = (System.Windows.Forms.MaskedTextBox)windowsFormsHost1.Child;
+            PhoneNumbers.Add(aBox.Text);
+            aBox.Clear();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            aDialog = new System.Windows.Forms.SaveFileDialog();
+            aDialog.Filter = "Text Files | *.txt";
+            aDialog.ShowDialog();
+            System.IO.StreamWriter myWriter = new System.IO.StreamWriter(aDialog.FileName, true);
+            foreach (string s in PhoneNumbers)
+                myWriter.WriteLine(s);
+            myWriter.Close();
+
+        }
+    }
+}
---
+++ WPF/WpfUserControl/WpfUserControl/ShowNumberControl.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfUserControl
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowNumberControl.xaml
+    /// </summary>
+    public partial class ShowNumberControl : UserControl
+    {
+        //private int currNumber = 0;
+        //public int CurrentNumber
+        //{
+        //    get { return currNumber; }
+        //    set
+        //    {
+        //        currNumber = value;
+        //        // Передаем в метку текущее значение
+        //        numberDisplay.Content = CurrentNumber.ToString();
+        //    }
+        //}
+
+
+        public int CurrentNumber
+        {
+            get { return (int)GetValue(CurrentNumberProperty); }
+            set { SetValue(CurrentNumberProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CurrentNumber.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentNumberProperty =
+            DependencyProperty.Register("CurrentNumber", typeof(int), typeof(ShowNumberControl), new PropertyMetadata(100, new PropertyChangedCallback(CurrentNumberChanged)),
+                new ValidateValueCallback(ValidateCurrentNumber));
+
+        private static void CurrentNumberChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            ShowNumberControl s = (ShowNumberControl)depObj;
+            Label theLabel = s.numberDisplay;
+            theLabel.Content = args.NewValue.ToString();
+        }
+
+        public static bool ValidateCurrentNumber(object value)
+        {
+            if (Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 500)
+                return true;
+            else
+                return false;
+        }
+
+        public ShowNumberControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

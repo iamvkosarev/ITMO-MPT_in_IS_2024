7746876f7be6aa236cf4c590e08e7abe30bb60fc
Vsevolod Kosarev <iamvkosarev@gmail.com>
2024-04-12 01:18:09 +0800

    Сделал пр.9
    Кратко но полезно понять как можно выполнять сложные операции и возврщаться к потоку отображения

---
+++ WPF/BackgroundWorker/BackgroundWorker/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BackgroundWorker
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private System.ComponentModel.BackgroundWorker aWorker;
+        public MainWindow()
+        {
+            InitializeComponent();
+            aWorker = new System.ComponentModel.BackgroundWorker();
+            aWorker.WorkerSupportsCancellation = true;
+            aWorker.DoWork += aWorker_DoWork;
+            aWorker.RunWorkerCompleted += aWorker_RunWorkerCompleted;
+        }
+        private void aWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                for (int j = 1; j <= 10000000; j++)
+                {
+                }
+                if (aWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                UpdateDelegate update = new UpdateDelegate(UpdateLabel);
+                label1.Dispatcher.BeginInvoke(System.Windows.Threading.DispatcherPriority.Normal, update, i);
+            }
+        }
+        private void aWorker_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+                label2.Content = "Run Completed";
+            else
+                label2.Content = "Run Cancelled";
+        }
+
+        private delegate void UpdateDelegate(int i);
+        private void UpdateLabel(int i)
+        {
+            label1.Content = "Cycles: " + i.ToString();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            aWorker.RunWorkerAsync();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            aWorker.CancelAsync();
+        }
+    }
+}
